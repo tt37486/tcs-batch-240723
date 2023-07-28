7230c35679ad28911e82326453a82130a56f957a
tt37486 <67602953+tt37486@users.noreply.github.com>
2023-07-28 09:35:07 +0530

    Update tripti-javacode.java

--- tripti-javacode.java
+++ tripti-javacode.java
@@ -1,2 +1,2 @@
 public class HellowWorld{ public static void 
-                         main(String[] args){ System.out.println("Hello World");}}
+                         main (String[] args){ System.out.println("Hello World");}}
